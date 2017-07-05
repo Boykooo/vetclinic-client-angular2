22d2cd4ee383b3cf9ebbd9929c918b9cd08393af
Boykooo <Boykooo@bk.ru>
2017-07-05 15:52:12 +0300

    admin auth in progress

--- src/app/admin/admin-auth.component.ts
+++ src/app/admin/admin-auth.component.ts
@@ -1,11 +1,36 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 import {Employee} from "../entity/employee";
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'admin-auth',
   templateUrl: './admin-auth.component.html',
 })
 
-export class AdminAuthComponent {
-  private employee: Employee;
+export class AdminAuthComponent implements OnInit {
+
+  employee: Employee;
+  private token: string;
+  private pathToApi: string;
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  constructor(private http: Http) {
+  }
+
+  ngOnInit(): void {
+    this.pathToApi = "http://localhost:8080/api/auth/employee";
+    this.employee = new Employee();
+  }
+
+  login(): void {
+    this.http
+      .post(this.pathToApi,
+        this.employee,
+        this.headers
+      )
+      .toPromise()
+      .then(res => console.log(res));
+  }
+
+
 }
