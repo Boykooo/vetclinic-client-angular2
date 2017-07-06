45db69f224e8e122f622c607512e40c055c500a2
Boykooo <Boykooo@bk.ru>
2017-07-06 15:27:16 +0300

    logout from admin panel

--- src/app/admin/adminNavigation/admin-panel-navigation.component.ts
+++ src/app/admin/adminNavigation/admin-panel-navigation.component.ts
@@ -1,9 +1,21 @@
 import {Component} from '@angular/core'
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'admin-panel-navigation',
   templateUrl: './admin-panel-navigation.component.html'
 })
-export class AdminPanelNavigationComponent{
+export class AdminPanelNavigationComponent {
 
+  constructor(private authService: AuthService,
+              private router: Router) {
+
+  }
+
+  logout(): void {
+    console.log("invoke")
+    this.authService.logout();
+    this.router.navigate(['/admin']);
+  }
 }
